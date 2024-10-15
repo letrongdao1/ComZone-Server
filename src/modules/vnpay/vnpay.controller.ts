fedd18e98feb1fbba75a8133c9087df5c0fa7473
coude1 <letrongdaocontact8@gmail.com>
2024-10-15 14:38:23 +0700

    authorize-some-controllers[DaoLT]

--- src/modules/vnpay/vnpay.controller.ts
+++ src/modules/vnpay/vnpay.controller.ts
@@ -1,14 +1,24 @@
-import { Body, Controller, Get, Ip, Post, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Ip,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { VnpayService } from './vnpay.service';
 import { VNPayRequest } from './dto/vnp-payment-url-request';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../authentication/guards/jwt-auth.guard';
 
-@ApiTags('VNPay')
 @ApiBearerAuth()
+@ApiTags('VNPay')
 @Controller('vnpay')
 export class VnpayController {
   constructor(private readonly vnpayService: VnpayService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post('create_payment_link')
   createPaymentLink(@Body() vnpayRequest: VNPayRequest, @Ip() ip: string) {
     return this.vnpayService.createPaymentLink(vnpayRequest, ip);
