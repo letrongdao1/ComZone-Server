6d6a6e902537b4a1e135952e5eee63e803a5ce24
coude1 <letrongdaocontact8@gmail.com>
2025-01-04 17:51:35 +0700

    update swaggerUI[DaoLT]

--- src/modules/ai-integration/ai-integration.controller.ts
+++ src/modules/ai-integration/ai-integration.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AiIntegrationService } from './ai-integration.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('AI Integration')
 @Controller('ai-integration')
 export class AiIntegrationController {
   constructor(private readonly aiIntegrationService: AiIntegrationService) {}
--- src/modules/auction-config/auction-config.controller.ts
+++ src/modules/auction-config/auction-config.controller.ts
@@ -1,10 +1,25 @@
-import { Controller, Get, Post, Put, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Body,
+  Param,
+  UseGuards,
+} from '@nestjs/common';
 import { AuctionConfigService } from './auction-config.service';
 import {
   CreateAuctionConfigDto,
   UpdateAuctionConfigDto,
 } from './dto/auction-config.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from '../authorization/roles.decorator';
+import { Role } from '../authorization/role.enum';
+import { PermissionsGuard } from '../authorization/permission.guard';
+import { JwtAuthGuard } from '../authentication/guards/jwt-auth.guard';
 
+@ApiBearerAuth()
+@ApiTags('Auction config')
 @Controller('auction-config')
 export class AuctionConfigController {
   constructor(private readonly auctionConfigService: AuctionConfigService) {}
@@ -19,9 +34,12 @@ export class AuctionConfigController {
     return await this.auctionConfigService.getConfig();
   }
 
-  @Put(':id') // :id will be the identifier for the AuctionConfig
+  @Roles(Role.ADMIN)
+  @UseGuards(PermissionsGuard)
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
   async updateConfig(
-    @Param('id') auctionConfigId: string, // Get the id from the URL parameter
+    @Param('id') auctionConfigId: string,
     @Body() updateAuctionConfigDto: UpdateAuctionConfigDto,
   ) {
     return await this.auctionConfigService.updateConfig(
